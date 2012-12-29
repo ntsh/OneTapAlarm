f29b04cb3be558878f9221f38a1610c2b00157bb
Neetesh Gupta <neetesh.iitian@gmail.com>
2012-12-29 16:15:38 +0530

    Initial Commit

---
+++ OneTapAlarm/NGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NGAppDelegate.h
+//  OneTapAlarm
+//
+//  Created by Neetesh Gupta on 29/12/12.
+//  Copyright (c) 2012 Neetesh Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OneTapAlarm/NGViewController.h
@@ -0,0 +1,13 @@
+//
+//  NGViewController.h
+//  OneTapAlarm
+//
+//  Created by Neetesh Gupta on 29/12/12.
+//  Copyright (c) 2012 Neetesh Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NGViewController : UIViewController
+
+@end
