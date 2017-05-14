c43b05c8e5ed25d0b2ab542afa4962c1134370c1
Neetesh Gupta <neetesh.ng@gmail.com>
2017-05-14 23:06:26 +0100

    Add event tracking via flurry
    Also, removed flurry-config.plist file to be tracked for changes
    using command :
    git update-index --assume-unchanged
    
    To see list of files not being tracked:
    git ls-files -v | grep -e "^[hsmrck]"

---
+++ OneTapAlarm/Helpers/NGAnalytics.h
@@ -0,0 +1,13 @@
+//
+//  NGAnalytics.h
+//  OneTapAlarm
+//
+//  Created by NG on 14/05/17.
+//  Copyright © 2017 Neetesh Gupta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NGAnalytics : NSObject
+
+@end
---
+++ OneTapAlarm/Helpers/NGAnalytics.m
@@ -0,0 +1,13 @@
+//
+//  NGAnalytics.m
+//  OneTapAlarm
+//
+//  Created by NG on 14/05/17.
+//  Copyright © 2017 Neetesh Gupta. All rights reserved.
+//
+
+#import "NGAnalytics.h"
+
+@implementation NGAnalytics
+
+@end
